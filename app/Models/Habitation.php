bb2fc6a30ee4a250126a9033755019c635936851
Daniel Leussa <leussadaniel04@gmail.com>
2023-08-17 15:26:31 +0200

    add relations between user and habitation

--- app/Models/Habitation.php
+++ app/Models/Habitation.php
@@ -4,11 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
 class Habitation extends Model
 {
     use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * Get the user that owns the Habitation
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 
 }
